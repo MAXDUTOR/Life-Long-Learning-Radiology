ac5fa5b017314f8de65c770cb025f7d9cd04fb36
MAXDUTOR <dreamerslam@gmail.com>
2024-05-03 16:00:32 +0700

    update course 3 part 3

---
+++ course-3/part-3/__/firebase/init.js
@@ -0,0 +1,9 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyB761OyLK0UlOJGcbUy8ipyzPqPx6aA1mg",
+  "authDomain": "life-long-4-course-3-part-3.firebaseapp.com",
+  "databaseURL": "",
+  "messagingSenderId": "491073203820",
+  "projectId": "life-long-4-course-3-part-3",
+  "storageBucket": "life-long-4-course-3-part-3.appspot.com"
+});
